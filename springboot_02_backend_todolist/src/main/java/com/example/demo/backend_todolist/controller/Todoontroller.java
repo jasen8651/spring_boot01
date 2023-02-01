f00a79ac74008cff87c55effe4a376938a8c1391
jasen8651 <jasen3644@naver.com>
2023-02-01 15:53:02 +0900

    spring

--- springboot_02_backend_todolist/src/main/java/com/example/demo/backend_todolist/controller/Todoontroller.java
+++ springboot_02_backend_todolist/src/main/java/com/example/demo/backend_todolist/controller/Todoontroller.java
@@ -11,21 +11,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.backend_todolist.dto.TodoDTO;
 import com.example.demo.backend_todolist.service.TodoServerice;
-
-import lombok.RequiredArgsConstructor;
-
+//스픵부트
 //@RequiredArgsConstructor =@Controller + @ResponseBody
 // http://localhost:8090/todo/all
 @RestController
@@ -58,8 +54,8 @@ public class Todoontroller {
 		
 		if(chk>=1) {
 			//return new ResponseEntity<Object>(headers,HttpStatus.OK);
-			//return new ResponseEntity<Object>(HttpStatus.OK);
-			return new ResponseEntity<Object>(map, headers, HttpStatus.OK);
+			return new ResponseEntity<Object>(HttpStatus.OK);
+			//return new ResponseEntity<Object>(map, headers, HttpStatus.OK);
 			//return new ResponseEntity<Object>(map, HttpStatus.OK);
 		}else {
 			return new ResponseEntity<Object>(HttpStatus.NOT_ACCEPTABLE);
